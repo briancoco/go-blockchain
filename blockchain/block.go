e857f9e8fa85feb4cf41a9570192bfd0913bf1e2
briancoco <briannguyencoco@gmail.com>
2024-12-21 11:41:11 -0600

    blockchain prototype

---
+++ blockchain/block.go
@@ -0,0 +1,55 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+type Blockchain struct {
+	Blocks []*Block
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{
+		Timestamp:     time.Now().Unix(),
+		Data:          []byte(data),
+		PrevBlockHash: prevBlockHash,
+		Hash:          []byte{},
+	}
+	block.SetHash()
+	return block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.Blocks[len(bc.Blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
+
+// Blockchain needs an inital "Genesis" block to start
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{
+		Blocks: []*Block{NewGenesisBlock()},
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-blockchain/blockchain"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	bc := blockchain.NewBlockchain()
+
+	bc.AddBlock("First Block")
+	bc.AddBlock("Second Block")
+	bc.AddBlock("Third Block")
+
+	//traverse and print blocks
+	for _, block := range bc.Blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
 }
